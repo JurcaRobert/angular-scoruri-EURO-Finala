9c2cad5c824dfe7edb6ef93c22399047fa032c95
JurcaRobert <54647983+JurcaRobert@users.noreply.github.com>
2021-07-30 10:10:15 -0700

    Update finala.component.ts

--- src/app/finala/finala.component.ts
+++ src/app/finala/finala.component.ts
@@ -10,7 +10,7 @@ export class FinalaComponent implements OnInit {
   public flagEngland:string ="//ssl.gstatic.com/onebox/media/sports/logos/DTqIL8Ba3KIuxGkpXw5ayA_48x48.png";
   public flagItalia:string ="//ssl.gstatic.com/onebox/media/sports/logos/joYpsiaYi4GDCqhSRAq5Zg_48x48.png";
    public islogIn:boolean = false;
-  //  public seSterge:boolean = false;
+
     constructor() { }
    
     ngOnInit(): void {
@@ -22,10 +22,7 @@ export class FinalaComponent implements OnInit {
     public close():void{
       this.islogIn = false;
     }
-    // public sterge():void{
-    //   this.seSterge = true;
-    // }
-  
+
   }
   
   
